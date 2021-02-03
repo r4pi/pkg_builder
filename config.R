4e3a2092f8a740d5ab655fabae52c16243f27c37
Mark Sellors <github@5vcc.com>
2021-02-03 23:21:28 +0000

    abstracted some common config out

---
+++ config.R
@@ -0,0 +1,8 @@
+# configuration settings
+
+# local library path where libraries for r4pi get installed
+conf_local_libpath <- "~/R/r4pi"
+
+# pkg repo director location
+conf_binrepo_dir <- "pkgbinrepo/src/contrib"
+

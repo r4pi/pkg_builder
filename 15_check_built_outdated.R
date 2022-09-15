33f3ea2cafa374bdd6808c481ff9259dd3f18452
Mark Sellors <r4pi@5vcc.com>
2022-09-15 12:33:57 +0100

    minor build improvements

--- 15_check_built_outdated.R
+++ 15_check_built_outdated.R
@@ -3,6 +3,12 @@
 # Checks for packages that were built with an older
 # version of R and removes them
 
+
+# You can burn the entire local package library with:
+# remove.packages(installed.packages(lib.loc="~/R/r4pi")[,"Package"], lib="~/R/r4pi")
+
+cat("Checking for outdated builds of packages\n")
+
 CURRENT_R_VERSION <- paste(R.Version()[c("major", "minor")], collapse = ".")
 
 INSTALLED_PACKAGES <- installed.packages(lib.loc="~/R/r4pi")

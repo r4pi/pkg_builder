2cc38d162c09e8a1fb83578ade234f50f734700a
Mark Sellors <r4pi@5vcc.com>
2022-10-03 12:42:31 +0100

    Adds the architecture to the sent message

--- run_cron.sh
+++ run_cron.sh
@@ -3,6 +3,8 @@
 
 date
 
+ARCH=$(uname -m)
+
 LOCAL_AWS_CF_DIST_ID=${AWS_CF_DIST_ID:-default}
 
 if [ $LOCAL_AWS_CF_DIST_ID == "default" ]; then
@@ -17,19 +19,18 @@ make all
 
 # Set appropriate message and priority
 if [ $? == 0 ];then
-	MESSAGE="Package build successful"
+	MESSAGE="(${ARCH}) "
 	PRIORITY=0
-	NUM_PACKAGES="
-Number of packages this build: $(grep "tar.gz" sync.log | wc -l)"
+	NUM_PACKAGES="Number of packages this build: $(grep "tar.gz" sync.log | wc -l)"
 else
-	MESSAGE="Package build failed!"
+	MESSAGE="(${ARCH}) Package build failed!"
 	PRIORITY=1
 fi
 
 echo "Send the message via pushover"
 curl -s \
-  --form-string "token=aght6r9rv9qhivo8jdqu2awgbacsjt" \
-  --form-string "user=uk1mrore93mx7rhftgm1d96atndxvu" \
+  --form-string "token=${PUSHOVER_TOKEN}" \
+  --form-string "user=${PUSHOVER_USER}" \
   --form-string "message=${MESSAGE}${NUM_PACKAGES}" \
   --form-string "priority=${PRIORITY}" \
   https://api.pushover.net/1/messages.json

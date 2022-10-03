207cbf143f430afd633498e3f88b6906621dda68
Mark Sellors <r4pi@5vcc.com>
2022-10-03 13:01:29 +0100

    Update the cloudfront cache invalidation for multi-arch builds

--- 80_invalidate_cloudfront_cache.sh
+++ 80_invalidate_cloudfront_cache.sh
@@ -3,13 +3,16 @@
 set -euo pipefail
 
 INTERNAL_AWS_CF_DIST_ID=${AWS_CF_DIST_ID:-default}
+ARCH=$(uname -m)
 
 if [ "${INTERNAL_AWS_CF_DIST_ID}" == "default" ]; then
   echo 'Error: please ensure the AWS CloudFront Distribution ID is set in $AWS_CF_DIST_ID'
   exit 1
 fi
 if grep index.html sync.log; then
-  aws cloudfront create-invalidation --distribution-id "${AWS_CF_DIST_ID}" --paths /index.html
+    aws cloudfront create-invalidation \
+        --distribution-id "${AWS_CF_DIST_ID}" \
+        --paths /${ARCH}/index.html /${ARCH}/src/contrib/PACKAGES /${ARCH}/src/contrib/PACKAGES.gz
 else
 	echo "Invalidation not required"
 fi

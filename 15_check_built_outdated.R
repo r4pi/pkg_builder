d393e82402559ce9f9abe96f41e8f81daeafcb5c
Mark Sellors <r4pi@5vcc.com>
2022-10-03 12:32:38 +0100

    Simple script to install all r4pi packages

--- 15_check_built_outdated.R
+++ 15_check_built_outdated.R
@@ -9,6 +9,8 @@
 
 cat("Checking for outdated builds of packages\n")
 
+source("config.R")
+
 CURRENT_R_VERSION <- paste(R.Version()[c("major", "minor")], collapse = ".")
 
 INSTALLED_PACKAGES <- installed.packages(lib.loc="~/R/r4pi")
@@ -26,4 +28,19 @@ VERSIONS_MATCH <- unlist(lapply(INSTALLED_PACKAGES[,"Built"], version_mismatch))
 OUTDATED_PACKAGES <- INSTALLED_PACKAGES[,"Package"][VERSIONS_MATCH]
 
 remove.packages(OUTDATED_PACKAGES, lib="~/R/r4pi")
+INSTALLED_PACKAGES[,"Package"][VERSIONS_MATCH]
+
+pkgs_versions <- data.frame(
+  "Package" = INSTALLED_PACKAGES[,"Package"][VERSIONS_MATCH],
+  "Version" = INSTALLED_PACKAGES[,"Version"][VERSIONS_MATCH]
+)
+
+marked_for_removal <- apply(pkgs_versions, 1, function(x){paste0(x[1], "_", x[2], ".tar.gz")})
+
+for (package in marked_for_removal){
+  # cat(package)
+	pkg_full_path <- file.path(conf_binrepo_dir, package)
+	cat("Removing file: ", pkg_full_path, "\n")
+	unlink(pkg_full_path)
+}
 
---
+++ install_all_r4pi.R
@@ -0,0 +1,2 @@
+install.packages(available.packages(repos="file:///home/mds/r4pi/packages/pkgbinrepo/aarch64")[,"Package"], repos="file:///home/mds/r4pi/packages/pkgbinrepo/aarch64", lib="~/R/r4pi")
+

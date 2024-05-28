2e34d0c47b31ab1913df5b1fb080db3177148ad8
Mark Sellors <git@5vcc.com>
2024-05-28 16:29:56 +0100

    UPdates to support ubuntu

--- 75_repo_sync.sh
+++ 75_repo_sync.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 
 if [ $(hostname) == "buildberry3" ]; then
     s3cmd sync ./pkgbinrepo/bookworm/ s3://pkgs.r4pi.org/bookworm/ | tee sync.log
+elif [ $(hostname) == "buildberry4" ]; then
+    s3cmd sync ./pkgbinrepo/aarch64/ s3://pkgs.r4pi.org/noble/ | tee sync.log
 else
     s3cmd sync ./pkgbinrepo/ s3://pkgs.r4pi.org/ | tee sync.log
 fi

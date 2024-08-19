f16193613224069465fdfe7bc1c7aa5113869181
Mark Sellors <github@5vcc.com>
2024-08-19 11:01:32 +0100

    Fine tuning

--- 60_pkg_write_packages.R
+++ 60_pkg_write_packages.R
@@ -10,13 +10,14 @@ previous_all_built_pkgs <- if( file.exists("all_built_pkgs.txt")){
 }
 
 current_all_built_pkgs <- dir(conf_binrepo_dir)
+is_not_pkg_override <- is.na(Sys.getenv("PKG_OVERRIDE", unset = NA))
 
-if ( identical(current_all_built_pkgs, previous_all_built_pkgs)){
+if ( identical(current_all_built_pkgs, previous_all_built_pkgs) && is_not_pkg_override){
     cat("-- No changes since last run... skipping\n", file = stdout())
 } else {
     writeLines(current_all_built_pkgs, "all_built_pkgs.txt")
     setwd(conf_binrepo_dir)
     # tools::write_PACKAGES(type = "source", verbose = TRUE)
-    tools::update_PACKAGES(type = "source", verbose = TRUE)
+    tools::update_PACKAGES(type = "source", verbose.level = 1)
 }
 
